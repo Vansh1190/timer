7244e28705a218de53bbd0a7a3219f56a70a14ca
Vansh <77226791+Vansh1190@users.noreply.github.com>
2023-07-05 12:14:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// FlipCardAnim '\r\n'
+// FlipCardAnim
 const FlipCardAnim = (i, flipCard) => {
-	// eslint-disable-line
   const c = document.getElementById('flipCardSec').children;
   const topHalf = c[0];
   const bottom = c[1];
@@ -8,22 +7,22 @@ const FlipCardAnim = (i, flipCard) => {
   const BottomFlip = document.createElement('div');
   topFlip.classList.add('top-flip');
   BottomFlip.classList.add('bottom-flip');
+  let tempI = i;
   if (i - 1 === -2) {
-    // eslint-disable-next-line no-param-reassign
-    i = 59;
+    tempI = 59;
   }
-  topHalf.textContent = (i - 1 < 9) ? `0${i}` : i;
-  bottom.textContent = (i < 9) ? `0${i}` : i;
-  topFlip.textContent = (i - 1 < 9) ? `0${i}` : i - 1;
-  BottomFlip.textContent = (i < 9) ? `0${i}` : i;
+  topHalf.textContent = (tempI - 1 < 9) ? `0${tempI}` : tempI;
+  bottom.textContent = (tempI < 9) ? `0${tempI}` : tempI;
+  topFlip.textContent = (tempI - 1 < 9) ? `0${tempI}` : tempI - 1;
+  BottomFlip.textContent = (tempI < 9) ? `0${tempI}` : i;
   topFlip.addEventListener('animationstart', () => {
-    topHalf.textContent = (i - 1 < 9) ? `0${i}` : i - 1;
+    topHalf.textContent = (tempI - 1 < 9) ? `0${tempI}` : tempI - 1;
   });
   topFlip.addEventListener('animationend', () => {
     topFlip.remove();
   });
   BottomFlip.addEventListener('animationend', () => {
-    bottom.textContent = (i - 1 < 9) ? `0${i}` : i - 1;
+    bottom.textContent = (tempI - 1 < 9) ? `0${tempI}` : tempI - 1;
     BottomFlip.remove();
   });
   flipCard.appendChild(topFlip);
@@ -43,15 +42,6 @@ const MinCARD = (document.getElementById('flipCardMin'));
 const HrCARD = (document.getElementById('flipCardHour'));
 const DayCARD = (document.getElementById('flipCardDay'));
 
-/* eslint-disable no-multiple-empty-lines */
-
-// used for disabling multiple lines
-
-
-
-
-/* eslint-enable no-multiple-empty-lines */
-
 const loop = setInterval(() => {
   const date = new Date().getTime();
   const distance = LaunchDate - date;
@@ -61,15 +51,10 @@ const loop = setInterval(() => {
   const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   if (days < 0 || hours < 0 || minutes < 0 || seconds < 0) {
-  // document.getElementById('second').style.animation = 'none';
     clearInterval(loop);
 
     return;
   }
-
-/*eslint-disable */
-	
-  /* eslint-enable */
 
   if (minutes !== 0 && seconds === 59) {
     FlipCardAnim(minutes + 1, MinCARD);
@@ -90,13 +75,16 @@ const loop = setInterval(() => {
     document.getElementById('second').innerHTML = '00';
   }
 
-  /* eslint-disable no-unused-expressions */
-  (days < 10) ? days = `0${days}` : null;
-  (hours < 10) ? hours = `0${hours}` : null;
-  (minutes < 10) ? minutes = `0${minutes}` : null;
-  // (seconds < 10) ? seconds = '0' +  seconds: null
-  /* eslint-enable no-unused-expressions */
+  if (days < 10) {
+    days = `0${days}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   document.getElementById('secTop').innerHTML = seconds;
   document.getElementById('secBottom').innerHTML = seconds;
   FlipCardAnim(seconds, SecCARD);
